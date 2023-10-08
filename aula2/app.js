d7a4656f76c3545cdd91dabf7b48c9a609e6bd05
Alan Francis <alanfrancisangra@gmail.com>
2023-10-08 18:30:46 -0300

    Aula 2

--- aula2/app.js
+++ aula2/app.js
@@ -1,12 +1,9 @@
 const multiplicacao = require('./modulos/mod1');
 const path = require('path');
 
-console.log(multiplicacao(2, 2))
+console.log(multiplicacao(2, 2));
 
+console.log(__filename);
+console.log(__dirname);
 
-
-console.log(__filename)
-console.log(__dirname)
-
-
-console.log(path.resolve(__dirname, '..', '..'))
+console.log(path.resolve(__dirname, '..', '..'));
